9d54f12c2ee740182c08ddbbf9b6fcd5c3abe8f4
Kenny Trinh <23616@student.dorset-college.ie>
2021-04-25 16:47:58 +0200

    Listing all bike stations from Dublin and Paris in RecyclerView

--- app/src/main/java/com/example/bikeassistant/BikeStationsFragment.kt
+++ app/src/main/java/com/example/bikeassistant/BikeStationsFragment.kt
@@ -1,10 +1,19 @@
 package com.example.bikeassistant
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.fragment_bike_stations.*
+import okhttp3.*
+import timber.log.Timber
+import java.io.IOException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +26,75 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class BikeStationsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    var bikeStations = ArrayList<BikeStation>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView_bike_stations.apply {
+            layoutManager = LinearLayoutManager(activity)
+
+            // todo: extract strings to strings.xml
+
+
+            var contractNames = listOf("dublin", "cergy-pontoise", "creteil")
+            var apiKey = "d698bd9f3088ba5f431482fafabc3abd5199ead4"
+            var urls = mutableListOf<String>()
+            for (contractName in contractNames) {
+                // example of dublin bike stations: https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=d698bd9f3088ba5f431482fafabc3abd5199ead4
+                var url = "https://api.jcdecaux.com/vls/v1/stations?contract=" + contractName + "&apiKey=" + apiKey
+                urls.add(url)
+            }
+            fetchAllJson(urls)
         }
     }
 
+    // todo: separate bike stations into cities (dublin has a single url, paris has two urls)
+    // todo: refactor nested lines function
+    private fun fetchAllJson(urls: List<String>) {
+        Timber.i("Fetching json data from API")
+        for (url in urls) {
+            var request = Request.Builder().url(url).build()
+            var client = OkHttpClient()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Timber.i("Failed to load JSON data")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val body = response?.body?.string()
+                    val gson = GsonBuilder().create()
+                    val type = object : TypeToken<ArrayList<BikeStation>>() {}.type
+                    bikeStations.addAll(gson.fromJson(body, type))
+
+                    if (url == urls[urls.lastIndex]) {
+                        bikeStations.sortBy { bikeStation -> bikeStation.name }
+                        runOnUiThread {
+                            recyclerView_bike_stations.adapter = BikeStationsAdapter(bikeStations)
+                        }
+                    }
+
+                    Timber.i("JSON data successfully loaded")
+                }
+
+            })
+        }
+    }
+
+
+    // to run on main thread
+    private fun runOnUiThread(task: Runnable) {
+        Handler(Looper.getMainLooper()).post(task)
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_bike_stations, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BikeStationsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BikeStationsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
